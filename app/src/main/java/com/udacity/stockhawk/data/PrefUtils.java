9593984ec4862ffe8d8cfe861f207a420018c97e
waszak <kwaszak67@gmail.com>
2017-03-18 20:57:06 +0100

    fix: Solve issue with adding/editing stock preferences.

--- app/src/main/java/com/udacity/stockhawk/data/PrefUtils.java
+++ app/src/main/java/com/udacity/stockhawk/data/PrefUtils.java
@@ -38,7 +38,7 @@ public final class PrefUtils {
 
     private static void editStockPref(Context context, String symbol, Boolean add) {
         String key = context.getString(R.string.pref_stocks_key);
-        Set<String> stocks = getStocks(context);
+        Set<String> stocks = new HashSet<>(getStocks(context));
 
         if (add) {
             stocks.add(symbol);
